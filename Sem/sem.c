8211362e03823355c2df08363ad3d5e214a10745
Nicola <nicolafebbrari@live.it>
2023-02-27 20:51:58 +0100

    init sem

---
+++ Sem/sem.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+sem_t s;
+void* T(void * arg) 
+{
+    sem_wait(&s);
+    printf("Welcome! \n");
+    sleep(2);
+    printf("Bye!\n");
+    sem_post(&s);
+}
+
+int main() 
+{
+    sem_init(&s, 0, 1);
+    pthread_t o1, o2;
+    printf("In a 1st Thread now...\n");
+    pthread_create(&o1,NULL,T,NULL);
+    sleep(2);
+    printf("In a 2nd Thread now...\n");
+    pthread_create(&o2,NULL,T,NULL);
+    pthread_join(o1,NULL);
+    pthread_join(o2,NULL);
+    sem_destroy(&s);
+    return 0;
+ }
